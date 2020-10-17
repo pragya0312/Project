7db90699e339b3862c3b25ba0fb5a423baea92c2
pragya0312 <33320870+pragya0312@users.noreply.github.com>
2020-10-17 11:17:48 -0700

    Update UC2_EmployeeWage.sh

--- UC2_EmployeeWage.sh
+++ UC2_EmployeeWage.sh
@@ -8,18 +8,18 @@ AttendanceValue=$((RANDOM%2))      #Checking whether attendence value is 0 or 1.
 
 echo "$AttendanceValue"            #Printing the attendence value
 
-EmpWagePerHr=20
+EmpFullDayHr=8
 
 if [ $AttendanceValue -eq 1 ]       #Condition
 then
 	echo "Employee is Present"
-	EmpHrs=20
+	EmpWagePerHr=20
 
 else
        	echo "Employee is absent"
 	EmpHrs=0
 fi
-salary=$((EmpWagePerHr*EmpHrs))     #Calculating salary of employee.
+salary=$((EmpWagePerHr*EmpFullDayHr))     #Calculating salary of employee.
 
 echo "The Salary Of Employee is :" $salary
 
